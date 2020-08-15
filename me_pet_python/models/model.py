d0ecf95cd1b9b0fccf704f12140fe0269e6537c6
Jongkuk Lim <Lim.JeiKei@gmail.com>
2020-08-15 14:32:26 +0900

    Refactor for help command

--- me_pet_python/models/model.py
+++ me_pet_python/models/model.py
@@ -21,6 +21,7 @@ def get_base_model(feature_file="dog_feats.csv", mp_size=8, base_model=tf.keras.
     feats = tf.reshape(feats, [-1, int(2048//mp_size)])
 
     model = base_model(input_shape=(224, 224, 3), include_top=False, weights='imagenet')
+    model.summary()
     feats_out = tf.keras.layers.GlobalAveragePooling2D()(model.output)
     feats_out = tf.nn.max_pool1d(tf.expand_dims(feats_out, axis=-1), int(mp_size*1.5), mp_size, padding='SAME')
     feats_out = tf.reshape(feats_out, [-1, int(2048//mp_size)])
@@ -35,5 +36,6 @@ def get_base_model(feature_file="dog_feats.csv", mp_size=8, base_model=tf.keras.
     out_layer = 1 - (out_layer / tf.reduce_max(out_layer, axis=-1, keepdims=True))
 
     model = tf.keras.models.Model(model.input, [out_idx, out_layer])
+    model.summary()
 
     return model, f_names
