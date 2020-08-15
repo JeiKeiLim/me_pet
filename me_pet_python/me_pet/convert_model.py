deac216c7725cb7e6d986b5a6ad7539a8ee5fd0f
Jongkuk Lim <Lim.JeiKei@gmail.com>
2020-08-15 15:17:25 +0900

    Change directory structure for deployment

--- me_pet_python/me_pet/convert_model.py
+++ me_pet_python/me_pet/convert_model.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 
 
 def convert_model_tfjs(feature_file, label_name, mp_size=8, base_model=tf.keras.applications.ResNet50V2,
-                       model_root="./export/tf_model", tfjs_model_root="../tfjs/tfjs_model/"):
+                       model_root="./export/tf_model", tfjs_model_root="../docs/tfjs_model/"):
     model, f_names = get_base_model(feature_file=feature_file, mp_size=mp_size, base_model=base_model)
 
     model.save(model_root, save_format="tf")
