681590c189cd5e5c53095ac967ff4f17b8265d7d
Jongkuk Lim <Lim.JeiKei@gmail.com>
2020-08-15 14:41:20 +0900

    Fix MaxPooling bug

--- me_pet_python/models/model.py
+++ me_pet_python/models/model.py
@@ -18,13 +18,13 @@ def get_base_model(feature_file="dog_feats.csv", mp_size=8, base_model=tf.keras.
     f_names = feats.values[:, 0]
     feats = feats.values[:, 1:].astype(np.float32)
     feats = tf.nn.max_pool1d(tf.expand_dims(feats, axis=-1), int(mp_size*1.5), mp_size, padding='SAME')
-    feats = tf.reshape(feats, [-1, int(2048//mp_size)])
+    feats = tf.reshape(feats, [-1, feats.shape[1]])
 
     model = base_model(input_shape=(224, 224, 3), include_top=False, weights='imagenet')
     model.summary()
     feats_out = tf.keras.layers.GlobalAveragePooling2D()(model.output)
     feats_out = tf.nn.max_pool1d(tf.expand_dims(feats_out, axis=-1), int(mp_size*1.5), mp_size, padding='SAME')
-    feats_out = tf.reshape(feats_out, [-1, int(2048//mp_size)])
+    feats_out = tf.reshape(feats_out, [-1, feats_out.shape[1]])
 
     out_layer = tf.expand_dims(feats_out, axis=1) - feats
     out_layer = tf.square(out_layer)
