d698c1c53c1193171618d7e2dcf8089adffa6979
Jongkuk Lim <Lim.JeiKei@gmail.com>
2020-08-15 14:55:46 +0900

    Change run method by config.json

--- me_pet_python/main.py
+++ me_pet_python/main.py
@@ -1,6 +1,7 @@
 import argparse
 from me_pet import *
 import tensorflow as tf
+import json
 
 if __name__ == "__main__":
     base_model_dict = {
@@ -10,31 +11,22 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("--mode", default="test", type=str, help="feature, filter, tfjs, test")
-    parser.add_argument("--gray", default=True, dest="gray", action="store_true", help="Convert image to gray for feature extraction")
-    parser.add_argument("--no-gray", dest="gray", action="store_false", help="Convert image to gray for feature extraction")
-    parser.add_argument("--model", default="resnetv2", type=str, help="resnetv2")
-    parser.add_argument("--img-root", default="", type=str, help="Image root directory")
-    parser.add_argument("--out-feat", default="./export/feature.csv", type=str, help="Feature result output/input path")
-    parser.add_argument("--out-filter", default="./export/filter.txt", type=str, help="Filter name list output/input path")
-    parser.add_argument("--out-label", default="./export/labels.txt", type=str, help="Label name list output/input path")
-    parser.add_argument("--out-model", default="./export/tf_model", type=str, help="TensorFlow Model output path")
-    parser.add_argument("--out-tfjs", default="../tfjs/tfjs_model/", type=str, help="TensorFlow JavaScript Model output path")
-    parser.add_argument("--filter-n-top", default=1000, type=int, help="Hyper-parameter for extracting filter labels")
-    parser.add_argument("--filter-n-test", default=3000, type=int, help="Hyper-parameter for extracting filter labels")
-    parser.add_argument("--filter-threshold", default=0.5, type=float, help="Hyper-parameter for extracting filter labels")
-    parser.add_argument("--mp-size", default=8, type=int, help="MaxPooling size")
+    parser.add_argument("--conf", default="./config.json", type=str, help="Configuration file path")
     args = parser.parse_args()
 
-    base_model = base_model_dict[args.model]
+    with open(args.conf, 'r') as f:
+        conf = json.load(f)
+
+    base_model = base_model_dict[conf['model']]
 
     if args.mode == "feature":
-        generate_features(args.img_root, args.out_feat, gray=args.gray, base_model=base_model)
+        generate_features(conf['img_root'], conf['out_feat'], gray=conf['gray'], base_model=base_model)
     elif args.mode == "filter":
-        generate_filter_list(args.out_feat, args.out_filter, n_top=args.filter_n_top, n_test=args.filter_n_test,
-                             threshold=args.filter_threshold, mp_size=args.mp_size, base_model=base_model)
+        generate_filter_list(conf['out_feat'], conf['out_filter'], n_top=conf['filter_n_top'], n_test=conf['filter_n_test'],
+                             threshold=conf['filter_threshold'], mp_size=conf['mp_size'], base_model=base_model)
     elif args.mode == "tfjs":
-        convert_model_tfjs(args.out_feat, args.out_label, mp_size=args.mp_size, model_root=args.out_model,
-                           tfjs_model_root=args.out_tfjs)
+        convert_model_tfjs(conf['out_feat'], conf['out_label'], mp_size=conf['mp_size'], model_root=conf['out_model'],
+                           tfjs_model_root=conf['out_tfjs'], base_model=base_model)
     elif args.mode == "test":
-        webcam_test(args.img_root, args.out_feat, args.out_filter, mp_size=args.mp_size, base_model=base_model, gray=args.gray)
-
+        webcam_test(conf['img_root'], conf['out_feat'], conf['out_filter'], mp_size=conf['mp_size'],
+                    base_model=base_model, gray=conf['gray'])
--- me_pet_python/me_pet/convert_model.py
+++ me_pet_python/me_pet/convert_model.py
@@ -1,9 +1,11 @@
 from models import get_base_model
 import tensorflowjs as tfjs
+import tensorflow as tf
 
 
-def convert_model_tfjs(feature_file, label_name, mp_size=8, model_root="./export/tf_model", tfjs_model_root="../tfjs/tfjs_model/"):
-    model, f_names = get_base_model(feature_file=feature_file, mp_size=mp_size)
+def convert_model_tfjs(feature_file, label_name, mp_size=8, base_model=tf.keras.applications.ResNet50V2,
+                       model_root="./export/tf_model", tfjs_model_root="../tfjs/tfjs_model/"):
+    model, f_names = get_base_model(feature_file=feature_file, mp_size=mp_size, base_model=base_model)
 
     model.save(model_root, save_format="tf")
     tfjs.converters.convert_tf_saved_model(model_root, tfjs_model_root)
